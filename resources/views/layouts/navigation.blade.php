ed1a4989857deb14e19890cf7825ee1a3abcd5f8
Md. Fazley Rabbi <fazley111@gmail.com>
2026-08-04 10:59:47 +0600

    feat: add Docs link to app top navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -40,6 +40,12 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
+
+                <!-- Docs Link -->
+                <a href="{{ route('docs.index') }}"
+                   class="hidden sm:inline-flex items-center px-3 py-1.5 text-sm font-medium text-gray-400 hover:text-gray-100 hover:bg-gray-800 rounded-lg transition">
+                    Docs
+                </a>
             </div>
 
             <!-- Right Side: User Dropdown -->
